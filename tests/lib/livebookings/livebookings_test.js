25fb331944414e650a555f9648deaa17e97d0b48
radical-edo <radical.3do@gmail.com>
2014-02-17 13:26:40 +0100

    this should be enough to make a request to livebookings API

--- tests/lib/livebookings/livebookings_test.js
+++ tests/lib/livebookings/livebookings_test.js
@@ -1,12 +1,12 @@
-var livebookings = require('../../../lib/livebookings/livebookings.js')(),
+var livebookings = require('../../../lib/livebookings/livebookings.js'),
     fs = require('fs'),
     xml2js = require('xml2js');
 
-describe('livebookings', function () {
+describe('bookings', function () {
   describe('buildXML', function () {
-
+    var bookings = livebookings();
     it('is a function', function () {
-      expect(livebookings.buildXML).to.be.a('function');
+      expect(bookings.buildXML).to.be.a('function');
     });
 
     describe('request body', function () {
@@ -20,11 +20,11 @@ describe('livebookings', function () {
       });
       describe('no need for converting', function () {
         before(function () {
-          livebookings.buildXML('SearchAvailabilityOneLocation', { SessionId: 'secret_id' });
+          bookings.buildXML('SearchAvailabilityOneLocation', { SessionId: 'secret_id' });
         });
 
         it('stores in a variable', function (done) {
-          parse(livebookings.body(), function (err, result) {
+          parse(bookings.body(), function (err, result) {
             result = JSON.stringify(result);
             result.should.eq(xml);
             done();
@@ -35,11 +35,11 @@ describe('livebookings', function () {
 
       describe('converts key', function () {
         before(function () {
-          livebookings.buildXML('SearchAvailabilityOneLocation', { session_id: 'secret_id' });
+          bookings.buildXML('SearchAvailabilityOneLocation', { session_id: 'secret_id' });
         });
 
         it('stores in a variable', function (done) {
-          parse(livebookings.body(), function (err, result) {
+          parse(bookings.body(), function (err, result) {
             result = JSON.stringify(result);
             result.should.eq(xml);
             done();
@@ -49,5 +49,18 @@ describe('livebookings', function () {
       });
     });
   });
-
+  describe('soapAction', function () {
+    var bookings = livebookings();
+    it('unknown', function () {
+      expect(bookings.action).to.throw('run buildXML beforehand');
+    });
+    describe('body has been build', function () {
+      before(function () {
+        bookings.buildXML('SearchAvailabilityOneLocation', { session_id: 'secret_id' });
+      });
+      it('has been stored', function () {
+        expect(bookings.action()).to.eq('http://schemas.livebookings.net/OneFormat/Aggregator/External/1/0/ExternalPortType/SearchAvailabilityOneLocationRequest');
+      });
+    });
+  });
 });
